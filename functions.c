af95f2b7bc399525f7698a6e0d21198ea0450117
Oivlisfriend <robertomarcelogongo@gmail.com>
2022-07-14 22:18:13 +0100

    Projecto

--- functions.c
+++ functions.c
@@ -4,20 +4,20 @@
 #include "declaration.h"
 #include <ctype.h>
 #include <string.h>
+
+//declaracao da estrutura de dados arvore
 struct tst{
 	char letra;
 	int ultimaletra;
 	struct tst *esquerda,*meio, *direita;
 };
+
+//funcao pra inicializar a arvore
 TST *inicializar(){
 	return NULL;
 }
-void limparpalavras(char *palavra){
-		int j;	
-	for (j=0;palavra[j]!='\0';j++){
-		palavra[j] = ' '; 		
-	}
-}
+
+//função para converter a string em minusculas
 void lowercase(char *palavra){
 	int j;	
 	for (j=0;palavra[j]!='\0';j++){
@@ -25,12 +25,15 @@ void lowercase(char *palavra){
 	}
 }
 
+// função que retorna o tamanho de uma string
 int vec_len(char *vec){
     int c = -1;
     while(*(vec++))
         c++;
     return c;
 }
+
+//função para inserir na raiz
 TST *inserirRaiz(char letra){
 	setlocale(LC_ALL,"");
 	TST *aux = (TST *) malloc(sizeof(TST));
@@ -45,6 +48,7 @@ TST *inserirRaiz(char letra){
 	return NULL;
 }
 
+//função para inserir filhos
 void inserirfilhos(TST **arvore, char *letra){
 			
 	if(!(*arvore))
@@ -60,6 +64,8 @@ void inserirfilhos(TST **arvore, char *letra){
 			(*arvore)->ultimaletra = 1;
 	}
 }
+
+//função para imprimir em pos-ordem
 void posordemimprimir(TST *arvore, char* palavra, int cont)
 {
 	if (arvore!=NULL)
@@ -83,6 +89,8 @@ void posordemimprimir(TST *arvore, char* palavra, int cont)
     else
     	printf("\t\t\t\t\t\t\t\t\tÁrvore vazia!!");
 }
+
+//Função para carregar o ficheiro das palavras
 void carregarpalavrasdoficheiro(TST **arvore){
 	FILE *ficha_f;
 	char palavra[10000];
@@ -94,6 +102,8 @@ void carregarpalavrasdoficheiro(TST **arvore){
 	}
 	fclose(ficha_f);
 }
+
+//função para imprimir em pre-ordem
 void preordemimprimir(TST *arvore, char* palavra, int cont)
 {
 	if (arvore!=NULL)
@@ -116,6 +126,8 @@ void preordemimprimir(TST *arvore, char* palavra, int cont)
     else
     	printf("\t\t\t\t\t\t\t\t\tÁrvore vazia!!");
 }
+
+//função para imprimir em-ordem
 void emordemimprimir(TST *arvore, char* palavra, int cont)
 {
     if (arvore!=NULL)
@@ -138,12 +150,16 @@ void emordemimprimir(TST *arvore, char* palavra, int cont)
     	printf("\t\t\t\t\t\t\t\t\tÁrvore vazia!!");
 }
 
+//função para para achar a maior palavra entre duas
 int maior_palavra_entre_duas(char *palavra,char *palavra1){
 		int  i,j,cont=0,numero=vec_len(palavra), numero1=vec_len(palavra1);
+		
 	if(numero>numero1){
 		for (i=0;palavra[i]!='\0'&&palavra[i]==palavra1[i];i++);
 		for (j=i;palavra[j]!='\0';j++)
 			cont++;
+		if(cont==0)
+			return -1;
 		return cont;
 	}
 	 	
@@ -151,10 +167,14 @@ int maior_palavra_entre_duas(char *palavra,char *palavra1){
 		for (i=0;palavra1[i]!='\0'&&palavra1[i]==palavra[i];i++);
 		for (j=i;palavra1[j]!='\0';j++)
 			cont++;
+		if(cont==0)
+			return -1;
 		return cont;
 	}
 	return j;
 }
+
+//função para calcular a distancia entre duas palavras
 int calculo_distancia_de_edicao(TST *arvore,char* palavra,char *palavra1){
 	int cont=-1,i;
 	if(buscar(arvore,palavra)&&buscar(arvore,palavra1)){
@@ -175,29 +195,33 @@ int calculo_distancia_de_edicao(TST *arvore,char* palavra,char *palavra1){
 		return cont;
 
 }
-void imprimir_palavras_com_distacia_de_edicao(TST *arvore, char* palavra,char *palavra1, int cont)
+
+//funcao para imprimir as palavras ligadas, ou palvras com os mesmos prefixos
+void imprimir_palavras_ligadas(TST *arvore, char* palavra,char *palavra1, int cont,int valor)
 {
 	int c;
     if (arvore!=NULL)
     {
     	if(arvore->esquerda!=NULL)
-     		imprimir_palavras_com_distacia_de_edicao(arvore->esquerda, palavra,palavra1, cont); 
+     		imprimir_palavras_ligadas(arvore->esquerda, palavra,palavra1, cont,valor); 
         palavra[cont] = arvore->letra;
         if (arvore->ultimaletra)
         {
             palavra[cont+1] = '\0';
             c=maior_palavra_entre_duas(palavra,palavra1);
-        	if(c>=0&&c<=3)
+        	if(c>=0&&c<=valor||!strcmp(palavra,palavra1))
             	printf( "%s \n", palavra);
         } 
         if(arvore->meio!=NULL)
-        	imprimir_palavras_com_distacia_de_edicao(arvore->meio, palavra,palavra1, cont+1);
+        	imprimir_palavras_ligadas(arvore->meio, palavra,palavra1, cont+1,valor);
         if(arvore->direita!=NULL)
-        	imprimir_palavras_com_distacia_de_edicao(arvore->direita, palavra,palavra1, cont);
+        	imprimir_palavras_ligadas(arvore->direita, palavra,palavra1, cont,valor);
     }
 }
 
+//função para verificar o maior prefixo entre as palavras
 char *maior_prefixo_comum(TST *arvore,char* letra, char* palavra,char* palavra1,char *palavra2,int cont){
+
 	if (arvore!=NULL&&(*letra)==(*palavra1)&&(*palavra1)==(*palavra2))
     {
 
@@ -208,7 +232,6 @@ char *maior_prefixo_comum(TST *arvore,char* letra, char* palavra,char* palavra1,
         if (arvore->ultimaletra||(*letra)!=(*palavra1)||(*palavra1)!=(*palavra2))
         {
             palavra[cont+1] = '\0';
-            printf( "%s",palavra);
         } 
         if(arvore->meio!=NULL)
         	maior_prefixo_comum(arvore->meio,letra+1,palavra,palavra1+1,palavra2+1,cont+1);
@@ -219,24 +242,24 @@ char *maior_prefixo_comum(TST *arvore,char* letra, char* palavra,char* palavra1,
     return palavra;
 }
 
-void buscarelemento(TST *arvore,char *palavra, char* letra,int cont)
-{
- if (arvore!=NULL)
-    {
-    	if(arvore->esquerda!=NULL&&arvore->letra>*letra)
-     		buscarelemento(arvore->esquerda, palavra,letra, cont);
-        	palavra[cont] = arvore->letra;
-        if (arvore->ultimaletra&&arvore->letra==*letra)
-        {
-            palavra[cont+1] = '\0';
-            printf( "%s\n", palavra);
-        } 
-        if(arvore->meio!=NULL&&arvore->letra==*letra)
-        	buscarelemento(arvore->meio, palavra,letra+1, cont+1);
+//funcao para procurar um elemento 
+void buscarelemento(TST *arvore,char *palavra, char* letra,int cont){
+	if (arvore!=NULL){
+	    if(arvore->esquerda!=NULL&&arvore->letra>*letra)
+	   		buscarelemento(arvore->esquerda, palavra,letra, cont);
+	       	palavra[cont] = arvore->letra;
+       if (arvore->ultimaletra&&arvore->letra==*letra){
+	        palavra[cont+1] = '\0';
+	        printf( "%s\n", palavra);
+	    } 
+	    if(arvore->meio!=NULL&&arvore->letra==*letra)
+	       	buscarelemento(arvore->meio, palavra,letra+1, cont+1);
         if(arvore->direita!=NULL&&arvore->letra<*letra)
-        	buscarelemento(arvore->direita, palavra,letra, cont);
-    }
+	    	buscarelemento(arvore->direita, palavra,letra, cont);
+	}   
 }
+
+//função para verificar palavras quebradas
 void palavra_quebrada(TST *arvore,char *palavra){
 	char palavra1[10000];
 	int i,x=0;
@@ -249,6 +272,8 @@ void palavra_quebrada(TST *arvore,char *palavra){
 	for(i=0;palavra[i]!='\0';i++)
 		buscarelemento(arvore,palavra1,palavra+i,0);
 }
+
+//funçao para procurar uma palavra
 int buscar(TST *arvore, char* letra)
 { 	
 	if(arvore==NULL)
@@ -266,8 +291,7 @@ int buscar(TST *arvore, char* letra)
 	}
 }
 
-
-
+//função para eliminar uma palavra
 int eliminar(TST *root, char* str)
 {
  
@@ -300,16 +324,19 @@ int eliminar(TST *root, char* str)
     }
     return 0;
 }
+
+//verifica se arvore está vazia
 int estavazia(TST *arvore){
 	if(arvore->esquerda||arvore->direita||arvore->meio)
 		return 0;
 	return 1;
 }
 
+// altera um elemento, eliminado o quue já existe
 void Alterar(TST *arvore, char *palavra, char *palavra1)
 {
-     	eliminar(arvore ,palavra);
-		inserirfilhos(&(arvore), palavra1);
+    eliminar(arvore ,palavra);
+	inserirfilhos(&(arvore), palavra1);
 
 }
 
