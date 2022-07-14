ed400e19f0c440ed0067b9a2b828f91901470c4b
Oivlisfriend <robertomarcelogongo@gmail.com>
2022-07-14 19:22:33 +0100

    Projecto

---
+++ functions.c
@@ -0,0 +1,315 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include "declaration.h"
+#include <ctype.h>
+#include <string.h>
+struct tst{
+	char letra;
+	int ultimaletra;
+	struct tst *esquerda,*meio, *direita;
+};
+TST *inicializar(){
+	return NULL;
+}
+void limparpalavras(char *palavra){
+		int j;	
+	for (j=0;palavra[j]!='\0';j++){
+		palavra[j] = ' '; 		
+	}
+}
+void lowercase(char *palavra){
+	int j;	
+	for (j=0;palavra[j]!='\0';j++){
+		palavra[j] = tolower(palavra[j]); 		
+	}
+}
+
+int vec_len(char *vec){
+    int c = -1;
+    while(*(vec++))
+        c++;
+    return c;
+}
+TST *inserirRaiz(char letra){
+	setlocale(LC_ALL,"");
+	TST *aux = (TST *) malloc(sizeof(TST));
+	if(aux){
+		aux->letra = letra;
+		aux->ultimaletra=0;
+		aux->meio=aux->direita=aux->esquerda=NULL;
+		return aux;
+	}
+	else
+		printf("Não foi possível alocar memória");
+	return NULL;
+}
+
+void inserirfilhos(TST **arvore, char *letra){
+			
+	if(!(*arvore))
+		*arvore=inserirRaiz(*letra);
+	if((*arvore)->letra<(*letra))
+		inserirfilhos(&((*arvore)->direita),letra);
+	else if((*arvore)->letra>(*letra))
+		inserirfilhos(&((*arvore)->esquerda),letra);
+	else{
+		if(*(letra+1))
+			inserirfilhos(&((*arvore)->meio),(letra+1));
+		else
+			(*arvore)->ultimaletra = 1;
+	}
+}
+void posordemimprimir(TST *arvore, char* palavra, int cont)
+{
+	if (arvore!=NULL)
+    {
+    		if(arvore->esquerda!=NULL)
+     		posordemimprimir(arvore->esquerda, palavra, cont); 
+	    if(arvore->direita!=NULL)
+        	posordemimprimir(arvore->direita, palavra, cont);
+        palavra[cont] = arvore->letra;
+        if (arvore->ultimaletra)
+        {
+            palavra[cont+1] = '\0';
+            if(vec_len(palavra)>=2)
+            	printf( "%s\n", palavra);
+        } 
+        if(arvore->meio!=NULL)
+        	posordemimprimir(arvore->meio, palavra, cont+1);
+    
+    
+    }
+    else
+    	printf("\t\t\t\t\t\t\t\t\tÁrvore vazia!!");
+}
+void carregarpalavrasdoficheiro(TST **arvore){
+	FILE *ficha_f;
+	char palavra[10000];
+	ficha_f=fopen("letras.txt","rt");
+	while(!feof(ficha_f)){
+		fscanf(ficha_f,"%s\n",palavra);
+		lowercase(palavra);
+		inserirfilhos(arvore, palavra);
+	}
+	fclose(ficha_f);
+}
+void preordemimprimir(TST *arvore, char* palavra, int cont)
+{
+	if (arvore!=NULL)
+    {
+     
+        palavra[cont] = arvore->letra;
+        if (arvore->ultimaletra)
+        {
+            palavra[cont+1] = '\0';
+            if(vec_len(palavra)>=2)
+            	printf( "%s\n", palavra);
+        } 
+       if(arvore->meio!=NULL)
+        	preordemimprimir(arvore->meio, palavra, cont+1);
+    	if(arvore->esquerda!=NULL)
+     		preordemimprimir(arvore->esquerda, palavra, cont); 
+	    if(arvore->direita!=NULL)
+        	preordemimprimir(arvore->direita, palavra, cont);
+    }
+    else
+    	printf("\t\t\t\t\t\t\t\t\tÁrvore vazia!!");
+}
+void emordemimprimir(TST *arvore, char* palavra, int cont)
+{
+    if (arvore!=NULL)
+    {
+    	if(arvore->esquerda!=NULL)
+     		emordemimprimir(arvore->esquerda, palavra, cont); 
+        palavra[cont] = arvore->letra;
+        if (arvore->ultimaletra)
+        {
+            palavra[cont+1] = '\0';
+            if(vec_len(palavra)>=2)
+            	printf( "%s\n", palavra);
+        } 
+        if(arvore->meio!=NULL)
+        	emordemimprimir(arvore->meio, palavra, cont+1);
+        if(arvore->direita!=NULL)
+        	emordemimprimir(arvore->direita, palavra, cont);
+    }
+    else
+    	printf("\t\t\t\t\t\t\t\t\tÁrvore vazia!!");
+}
+
+int maior_palavra_entre_duas(char *palavra,char *palavra1){
+		int  i,j,cont=0,numero=vec_len(palavra), numero1=vec_len(palavra1);
+	if(numero>numero1){
+		for (i=0;palavra[i]!='\0'&&palavra[i]==palavra1[i];i++);
+		for (j=i;palavra[j]!='\0';j++)
+			cont++;
+		return cont;
+	}
+	 	
+	else{
+		for (i=0;palavra1[i]!='\0'&&palavra1[i]==palavra[i];i++);
+		for (j=i;palavra1[j]!='\0';j++)
+			cont++;
+		return cont;
+	}
+	return j;
+}
+int calculo_distancia_de_edicao(TST *arvore,char* palavra,char *palavra1){
+	int cont=-1,i;
+	if(buscar(arvore,palavra)&&buscar(arvore,palavra1)){
+		cont++;
+		if(vec_len(palavra)>vec_len(palavra1)){
+			for(i=0;palavra[i]!='\0';i++)
+				if(palavra[i]!=palavra1[i])
+					cont++;
+		}
+			else{
+					for(i=0;palavra1[i]!='\0';i++)
+						if(palavra[i]!=palavra1[i])
+							cont++;
+			}
+		return cont;
+	}
+	else
+		return cont;
+
+}
+void imprimir_palavras_com_distacia_de_edicao(TST *arvore, char* palavra,char *palavra1, int cont)
+{
+	int c;
+    if (arvore!=NULL)
+    {
+    	if(arvore->esquerda!=NULL)
+     		imprimir_palavras_com_distacia_de_edicao(arvore->esquerda, palavra,palavra1, cont); 
+        palavra[cont] = arvore->letra;
+        if (arvore->ultimaletra)
+        {
+            palavra[cont+1] = '\0';
+            c=maior_palavra_entre_duas(palavra,palavra1);
+        	if(c>=0&&c<=3)
+            	printf( "%s \n", palavra);
+        } 
+        if(arvore->meio!=NULL)
+        	imprimir_palavras_com_distacia_de_edicao(arvore->meio, palavra,palavra1, cont+1);
+        if(arvore->direita!=NULL)
+        	imprimir_palavras_com_distacia_de_edicao(arvore->direita, palavra,palavra1, cont);
+    }
+}
+
+char *maior_prefixo_comum(TST *arvore,char* letra, char* palavra,char* palavra1,char *palavra2,int cont){
+	if (arvore!=NULL&&(*letra)==(*palavra1)&&(*palavra1)==(*palavra2))
+    {
+
+    	if(arvore->esquerda!=NULL)
+        	maior_prefixo_comum(arvore->esquerda,letra,palavra,palavra1,palavra2,cont);
+        	if(arvore->letra==*letra&&(*letra)==(*palavra1)&&(*palavra1)==(*palavra2))
+        		palavra[cont] = arvore->letra;
+        if (arvore->ultimaletra||(*letra)!=(*palavra1)||(*palavra1)!=(*palavra2))
+        {
+            palavra[cont+1] = '\0';
+            printf( "%s",palavra);
+        } 
+        if(arvore->meio!=NULL)
+        	maior_prefixo_comum(arvore->meio,letra+1,palavra,palavra1+1,palavra2+1,cont+1);
+        if(arvore->direita!=NULL)
+        	maior_prefixo_comum(arvore->direita,letra,palavra,palavra1,palavra2,cont);
+    }
+ 
+    return palavra;
+}
+
+void buscarelemento(TST *arvore,char *palavra, char* letra,int cont)
+{
+ if (arvore!=NULL)
+    {
+    	if(arvore->esquerda!=NULL&&arvore->letra>*letra)
+     		buscarelemento(arvore->esquerda, palavra,letra, cont);
+        	palavra[cont] = arvore->letra;
+        if (arvore->ultimaletra&&arvore->letra==*letra)
+        {
+            palavra[cont+1] = '\0';
+            printf( "%s\n", palavra);
+        } 
+        if(arvore->meio!=NULL&&arvore->letra==*letra)
+        	buscarelemento(arvore->meio, palavra,letra+1, cont+1);
+        if(arvore->direita!=NULL&&arvore->letra<*letra)
+        	buscarelemento(arvore->direita, palavra,letra, cont);
+    }
+}
+void palavra_quebrada(TST *arvore,char *palavra){
+	char palavra1[10000];
+	int i,x=0;
+	for(i=0;palavra[i]!='\0'&&x!=1;i++)
+		x=buscar(arvore,palavra+i);
+	if(x)
+		printf("Sim. ");
+	else
+		printf("Não.\n");
+	for(i=0;palavra[i]!='\0';i++)
+		buscarelemento(arvore,palavra1,palavra+i,0);
+}
+int buscar(TST *arvore, char* letra)
+{ 	
+	if(arvore==NULL)
+		 return 0;
+	if(arvore->letra<*letra)
+		buscar(arvore->direita,letra);
+	else if(arvore->letra>*letra)
+		buscar(arvore->esquerda,letra);
+	else{
+		if(*(letra+1)){
+			buscar(arvore->meio,(letra+1));
+		}
+		else
+			return arvore->ultimaletra;
+	}
+}
+
+
+
+int eliminar(TST *root, char* str)
+{
+ 
+    if (root == NULL)
+        return 0;
+    if (*(str+1)== '\0')
+    {
+        if (root->ultimaletra)
+        {
+            root->ultimaletra=0;
+            return estavazia(root);
+        }
+            return 0;
+    }
+    else
+    {
+        if (*str < root->letra)
+            eliminar(root->esquerda ,str);
+        else if (*str > root->letra)
+            eliminar(root->direita ,str);
+        else if (*str == root->letra)
+        {
+            if( eliminar(root->meio ,str+1 ) )
+            {
+                free(root->meio);
+                root->meio=NULL;
+                return !root->ultimaletra && estavazia(root);
+            }
+        }
+    }
+    return 0;
+}
+int estavazia(TST *arvore){
+	if(arvore->esquerda||arvore->direita||arvore->meio)
+		return 0;
+	return 1;
+}
+
+void Alterar(TST *arvore, char *palavra, char *palavra1)
+{
+     	eliminar(arvore ,palavra);
+		inserirfilhos(&(arvore), palavra1);
+
+}
+
---
+++ main.c
@@ -0,0 +1,152 @@
+#include "functions.c"
+
+int main(){
+	setlocale(LC_ALL,"");
+	TST *trie=inicializar();
+	char palavra[1000],palavra1[1000],palavra2[1000],letra[1000];
+	int opcao,i,distancia;
+	carregarpalavrasdoficheiro(&trie);
+	do{
+		system("cls");
+		printf("\t\t\t\t\t\t\tDigital Trie\n\n");
+		printf("\t\t\t**************************************************************************\n");
+		printf("\t\t\t#                                                                        #\n");
+		printf("\t\t\t#       1- Inserir                  2- Eliminar                          #\n");
+		printf("\t\t\t#       3- Palavra quebrada         4- Palavras ligadas                  #\n");
+		printf("\t\t\t#       5- Imprimir em pós-ordem    6- Imprimir em pré-ordem             #\n");
+		printf("\t\t\t#       7- Imprimir em ordem        8- Alterar elemento                  #\n");
+		printf("\t\t\t#       9- Distância de edição     10- Calcular a distância de edição    #\n");
+		printf("\t\t\t#       0- Sair\t\t\t\t                                 #\n");
+		printf("\t\t\t#                                                                        #\n");
+		printf("\t\t\t**************************************************************************\n");
+		printf("\t\t\tR:_ ");
+		scanf("%d",&opcao);
+		switch(opcao){
+			case 0:
+				printf("Terminando o programa...");
+				break;
+			case 1:
+				fflush(stdin);
+				printf("Digite a palavra a ser inserida na TRIE\n");
+				gets(palavra);
+				fflush(stdin);
+				lowercase(palavra);
+				if(!buscar(trie,palavra)){
+					inserirfilhos(&(trie), palavra);
+					printf("\t\t\t\t\t\t\tPalavra inserida na Trie!!");
+				}
+				else
+					printf("\t\t\t\t\t\t\t A palavra digitada já se encontra na Trie!!");	
+				break;
+			case 2:
+				fflush(stdin);
+				printf("Digite a palavra a ser eliminda na TRIE\n");
+				gets(palavra);
+				fflush(stdin);
+				lowercase(palavra);
+				eliminar(trie,palavra);
+				break;
+			case 3:// palavras quebras
+				fflush(stdin);
+				printf("\nPalavras Quebradas\n");
+				printf("Nesta sessão lhe serão mostradas as palavras quebras\n");
+				printf("Digite a primeira palavra\n");
+				gets(palavra);
+				lowercase(palavra);
+			 	palavra_quebrada(trie,palavra);
+				system("pause");
+				break;
+			case 4://Palavras ligadas
+				fflush(stdin);
+				printf("\nDigite as palavras que serão calculadas as suas distâncias de edição\n");
+				printf("Digite a primeira palavra\n");
+				gets(palavra);
+				fflush(stdin);
+				lowercase(palavra);
+				imprimir_palavras_com_distacia_de_edicao(trie, palavra1,palavra, 0);
+				system("pause");
+				break;
+			case 5://Impressão a pós-ordem
+				posordemimprimir(trie, palavra, 0);
+				system("pause");
+				break;
+			case 6://Impressão a pré-ordem
+				preordemimprimir(trie, palavra, 0);
+				system("pause");
+				break;
+				break;
+			case 7://Impressão a em-ordem
+				emordemimprimir(trie, palavra, 0);
+				system("pause");
+				break;
+			case 8://Alterar uma palavra
+				fflush(stdin);
+				printf("Digite a palavra a ser inserida na TRIE1\n");
+				gets(palavra);
+				fflush(stdin);
+				lowercase(palavra);
+				printf("Digite a palavra a ser inserida na TRIE\n");
+				gets(palavra1);
+				fflush(stdin);
+				lowercase(palavra1);
+				if(buscar(trie,palavra)){
+					if(!buscar(trie,palavra1)){
+						Alterar(trie,palavra,palavra1);
+
+					}else
+						printf("%s já existe na arvore",palavra1);
+				}
+				else
+					printf("%s não foi encontrada na árvore",palavra);
+				system("pause");
+
+				break;
+			case 9:
+				fflush(stdin);
+				printf("\nDigite as palavras que serão calculadas as suas distâncias de edição\n");
+				printf("Digite a primeira palavra\n");
+				gets(palavra);
+				lowercase(palavra);
+				fflush(stdin);
+				printf("Digite a segunda palavra\n");
+				gets(palavra1);
+				fflush(stdin);
+				lowercase(palavra1);
+				printf("Digite a terceira palavra\n");
+				gets(palavra2);
+				fflush(stdin);
+				lowercase(palavra2);
+				if(buscar(trie,palavra)&&buscar(trie,palavra1)&&buscar(trie,palavra2))
+					printf("A distância de edição entre a palavra %s, %s e %s é de : %s ",palavra,palavra1,palavra2, maior_prefixo_comum(trie,palavra,letra,palavra1,palavra2,0));
+				else
+					printf("Uma das palavras digitadas não consta na árvore");
+				system("pause");
+
+				break;
+			case 10:
+				printf("\nDigite as palavras que serão calculadas as suas distâncias de edição\n");
+				fflush(stdin);
+				printf("Digite a primeira palavra\n");
+				gets(palavra);
+				lowercase(palavra);
+				fflush(stdin);
+				printf("Digite a segunda palavra\n");
+				gets(palavra1);
+				fflush(stdin);
+				lowercase(palavra1);
+				distancia=calculo_distancia_de_edicao(trie,palavra,palavra1);
+				if(distancia!=-1)
+					printf("A distância de edição entre a palavra %s e %s é de : %d ",palavra,palavra1,distancia);
+				else
+					printf("Uma das palavras digitadas não existe na árvore");
+				system("pause");
+				break;
+			default:
+				printf("Opção inválida");
+				system("pause");
+
+		}
+	}while(opcao!=0);
+
+	return 0;
+}
